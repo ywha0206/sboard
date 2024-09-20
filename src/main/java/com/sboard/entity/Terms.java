2f8ac6c2df81256ee822808687323c14e8a46aa2
Your Name <you@example.com>
2024-09-20 17:16:04 +0900

    test 0920_4 0

--- src/main/java/com/sboard/entity/Terms.java
+++ src/main/java/com/sboard/entity/Terms.java
@@ -1,6 +1,7 @@
 package com.sboard.entity;
 
 import com.sboard.dto.TermsDTO;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
@@ -15,7 +16,9 @@ import lombok.*;
 @Table(name="terms")
 public class Terms {
     @Id
+    @Column(columnDefinition = "text(65535)", nullable = false)
     private String terms;
+    @Column(columnDefinition = "text(65535)", nullable = false)
     private String privacy;
 
     public TermsDTO toDTO() {
